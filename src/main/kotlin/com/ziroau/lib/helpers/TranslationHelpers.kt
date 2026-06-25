b9dc7f35ed1a063d9db3e43a804da4e39171a96c
Hudson <hudson@ziroau.com>
2026-06-25 22:24:26 +1000

    Add `translationKey` funcs

--- src/main/kotlin/com/ziroau/lib/helpers/TranslationHelpers.kt
+++ src/main/kotlin/com/ziroau/lib/helpers/TranslationHelpers.kt
@@ -6,10 +6,17 @@ import net.minecraft.text.Text.translatable
 
 object TranslationHelpers {
     fun translation(type: String, namespace: String, path: String): MutableText {
-        return translatable("$type.$namespace.$path")
+        return translatable(translationKey(type, namespace, path))
     }
 
     fun translation(type: String, id: Id): MutableText {
-        return translatable("$type.${id.namespace}.${id.path}")
+        return translatable(translationKey(type, id))
+    }
+
+    fun translationKey(type: String, id: Id): String {
+        return "$type.${id.namespace}.${id.path}"
+    }
+    fun translationKey(type: String, namespace: String, path: String): String {
+        return "$type.$namespace.$path"
     }
 }
